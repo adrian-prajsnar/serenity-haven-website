d5357b40068d6ed8a351af12f560e71e24379b37
adrian-prajsnar <adrian.prajsnar11@gmail.com>
2024-08-06 09:01:50 +0200

    Add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import { ReactNode } from 'react';
 import { Metadata } from 'next';
+import { NextFont } from 'next/dist/compiled/@next/font';
+import { Josefin_Sans } from 'next/font/google';
+
 import '@/app/_styles/globals.css';
 
 import Navigation from '@/app/_components/Navigation';
@@ -14,10 +17,17 @@ export const metadata: Metadata = {
     'Luxury hut hotel in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests.',
 };
 
+const josefin: NextFont = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
-      <body className='bg-primary-950 text-primary-100 min-h-screen'>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
