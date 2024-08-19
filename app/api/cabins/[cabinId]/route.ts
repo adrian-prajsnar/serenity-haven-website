d03c8572b08dc7b4911ecb7b53b22132867a8512
adrian-prajsnar <adrian.prajsnar11@gmail.com>
2024-08-19 08:40:37 +0200

    Add route handler for cabins/cabinId

---
+++ app/api/cabins/[cabinId]/route.ts
@@ -0,0 +1,19 @@
+import { getBookedDatesByCabinId, getCabin } from '@/app/_lib/data-service';
+
+export async function GET(
+  request: Request,
+  { params }: { params: { cabinId: string } }
+) {
+  const { cabinId } = params;
+
+  try {
+    const [cabin, bookedDates] = await Promise.all([
+      getCabin(Number(cabinId)),
+      getBookedDatesByCabinId(Number(cabinId)),
+    ]);
+
+    return Response.json({ cabin, bookedDates });
+  } catch {
+    return Response.json({ message: 'Cabin not found.' });
+  }
+}
